bd95ed1f2ceb8e714d1611146847e27a0d145be3
Miguel López <miguelopezv@gmail.com>
2026-04-19 01:04:22 -0500

    PieChart Fixes

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,4 +1,4 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
@@ -22,11 +22,13 @@ export default function PieChart({ results }: Props) {
           'rgba(255,  99, 132, 0.75)',
           'rgba(255, 159,  64, 0.75)',
           'rgba( 75, 192, 192, 0.75)',
+          'rgba(153, 102, 255, 0.75)',
         ],
         borderColor: [
           'rgba(255,  99, 132, 1)',
           'rgba(255, 159,  64, 1)',
           'rgba( 75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
         ],
         borderWidth: 1,
       },
