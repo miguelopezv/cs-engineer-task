e36ac65796e69891da43f4d5a2792dea7216a402
Miguel López <miguelopezv@gmail.com>
2026-04-19 01:03:34 -0500

    barrel file

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import BarChart from './BarChart';
+import BubbleChart from './BubbleChart';
+import LineChart from './LineChart';
+import PieChart from './PieChart';
+
+export { BarChart, BubbleChart, LineChart, PieChart };
